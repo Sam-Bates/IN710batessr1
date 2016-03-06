58bc6f5f74d08bfd145bf952d16c9f0f61221591
Sam-Bates <batez1993@gmail.com>
2016-03-07 10:52:30 +1300

    added Node class and created Stack skeleton

---
+++ Stack/Stack/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class Node
+    {
+        public String str { get; set; }
+        public Node next { get; set; }
+
+        public Node(String str)
+        {
+            this.str = str;
+            next = null;
+        }
+    }
+}
---
+++ Stack/Stack/Stack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class Stack
+    {
+        public Stack()
+        {
+        }
+        public void Push()
+        { 
+        }
+        public String Pop()
+        { 
+        }
+        public String Peek()
+        { 
+        }
+        public int Count()
+        { 
+        }
+        public bool IsEmpty()
+        { 
+        }
+    }
+}
