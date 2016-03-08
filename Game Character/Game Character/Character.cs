5bad01c6aa1eafcc86a18a90d5421a48c828e87d
Sam-Bates <batez1993@gmail.com>
2016-03-08 14:53:24 +1300

    initial game character commit

---
+++ Game Character/Game Character/Character.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Character
+{
+    abstract class Character
+    {
+        String Name;
+        //default weapon
+        public Character(String Name)
+        {
+            this.Name = Name;
+        }
+        public abstract String Declaim();
+    }
+}
---
+++ Game Character/Game Character/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_Character
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //IPayComputingMachine = IWeapon 
+            //useWeapon(), can change at any time, character needs to say different things depending on it's current weapon
+            //Employee = character class
+            //abstract method declaim()
+
+        }
+    }
+}
