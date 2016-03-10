303053f9201fce56b33f640acf6b9aaa1602a170
Sam-Bates <batez1993@gmail.com>
2016-03-10 23:23:32 +1300

    clock project started

---
+++ Time/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Time
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Timer tmr = new Timer();
+            tmr.Interval = 1000;//ticks every 1 second
+            tmr.Tick += new EventHandler(timer1_Tick);
+            tmr.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
