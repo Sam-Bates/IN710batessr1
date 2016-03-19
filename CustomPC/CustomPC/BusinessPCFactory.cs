04becc9876cd0f143414b5a99d692a82ec04709d
Sam-Bates <batez1993@gmail.com>
2016-03-19 13:39:05 +1300

    Implemented PCPart factories

---
+++ CustomPC/CustomPC/BusinessPCFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomPC
+{
+    class BusinessPCFactory : IPCMaker
+    {
+        public PCPart makeCPU()
+        {
+            PCPart currCPU = new BusinessCPU();
+            return currCPU;
+        }
+
+        public PCPart makeMemory()
+        {
+            PCPart currMem = new BusinessMemory();
+            return currMem;
+        }
+
+        public PCPart makeGPU()
+        {
+            PCPart currGPU = new BusinessGPU();
+            return currGPU;
+        }
+    }
+}
