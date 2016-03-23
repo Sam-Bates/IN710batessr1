c6015a9e4ef57b8697bf14ebdf6132dbd82bfed1
Sam-Bates <batez1993@gmail.com>
2016-03-23 14:44:22 +1300

    Project half finished, forgot to commit

---
+++ WeatherObserver/WeatherObserver/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeatherObserver
+{
+    public partial class Form1 : Form
+    {
+        Subject subject;
+        UnitObserver unitObserver;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            subject = new Subject();
+            unitObserver = new UnitObserver(listBox1, subject);
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            subject.UpdateValues(Int32.Parse(textBox1.Text), Int32.Parse(textBox2.Text), Int32.Parse(textBox3.Text));
+        }
+    }
+}
---
+++ WeatherObserver/WeatherObserver/Observer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeatherObserver
+{
+    public abstract class Observer : IObserver
+    {
+        protected double Temp;
+        protected double Humid;
+        protected double Pressure;
+        protected ListBox DisplayBox;
+        protected Subject Subject;
+        protected List<int> history;
+
+        public Observer(ListBox DisplayBox, Subject Subject)
+        {
+            this.DisplayBox = DisplayBox;
+            this.Subject = Subject;
+            Subject.AddObServer(this);
+        }
+        public Observer(ListBox DisplayBox, Subject Subject, List<int> history)
+        {
+            this.DisplayBox = DisplayBox;
+            this.Subject = Subject;
+            this.history = history;
+        }
+        public abstract void Update(int Temp, int Humid, int Pressure);
+
+        public virtual void Display()
+        {
+            DisplayBox.Items.Add("Temperature: " + Temp);
+            DisplayBox.Items.Add("Humidity: " + Humid);
+            DisplayBox.Items.Add("Pressure: " + Pressure);
+        }
+    }
+}
