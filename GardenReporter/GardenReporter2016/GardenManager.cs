15244a595bd6d0c4f915ca2d0830963eee32b2cd
Sam-Bates <batez1993@gmail.com>
2016-04-04 14:15:36 +1200

    Garden manager getters

---
+++ GardenReporter/GardenReporter2016/GardenManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GardenReporter2016
+{
+    public class GardenManager
+    {
+        List<Garden> gardens;
+        public GardenManager()
+        {
+            gardens = new List<Garden>();
+        }
+        public void AddGarden(Garden garden)
+        {
+            gardens.Add(garden);
+        }
+        public string GetGardenAreas()
+        {
+            string reportString = "";
+            for (int i = 0; i < gardens.Count; i++)
+            {
+                reportString += String.Format("{0,-14}:{1,8:f2}", gardens[i].OwnerName, gardens[i].GetArea() + "\n");
+                
+            }
+            return reportString;
+        }
+        public string GetOwnerBalances()
+        {
+            string reportString = "";
+            for (int i = 0; i < gardens.Count; i++)
+            {
+                reportString = String.Format("{0,-14}:{1,8:C2}", gardens[i].OwnerName, gardens[i].GetAccountBalance());
+            }
+            return reportString;
+        }
+    }
+}
