a9c930c998d15cd7a5eaed5e376e2e474b14f435
Sam-Bates <batez1993@gmail.com>
2016-04-04 12:08:21 +1200

    scorer class

--- QuizNight/QuizNight/Scorer.cs
+++ QuizNight/QuizNight/Scorer.cs
@@ -8,6 +8,17 @@ namespace QuizNight
 {
     public class Scorer
     {
-
+        public static int AdultScore(int correct, int incorrect)
+        {
+            int score = 0;
+            score = (correct * 10) - (incorrect * 1);
+            return score;
+        }
+        public static int ChildScore(int correct, int incorrect)
+        {
+            int score = 0;
+            score = (correct * 15);
+            return score;
+        }
     }
 }
