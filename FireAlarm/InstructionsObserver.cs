6f34259d681583c4b464a91bf630c67316abba26
Sam-Bates <batez1993@gmail.com>
2016-04-06 12:17:31 +1200

    subject and observer implemented

---
+++ FireAlarm/InstructionsObserver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireAlarm
+{
+    public class InstructionsObserver : FireAlarmObserverBase
+    {
+        public InstructionsObserver(FireAlarmSubject subject) : base (subject)
+        {
+
+        }
+        public override void FireAlarmHandlerMethod(object fireSubject, FireAlarmEventArgs fe)
+        {
+            String message = "Fire is " + fe.FireCategory.ToString() + ".";
+            switch (fe.FireCategory)
+            {
+                case EFireCategory.MINOR:
+                    message += " the fire extinguisher.";
+                    break;
+                case EFireCategory.SERIOUS:
+                    message += " call the fire department.";
+                    break;
+                case EFireCategory.INFERNO:
+                    message += " evacuate the building.";
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
