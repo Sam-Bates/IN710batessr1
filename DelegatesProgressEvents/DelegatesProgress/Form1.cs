8bb74c855b013e04deb1fc0db3b90fcebdc5e7d0
Sam-Bates <batez1993@gmail.com>
2016-04-07 18:47:57 +1200

    subject

--- DelegatesProgressEvents/DelegatesProgress/Form1.cs
+++ DelegatesProgressEvents/DelegatesProgress/Form1.cs
@@ -13,61 +13,40 @@ namespace DelegatesProgress
 {
     public partial class Form1 : Form
     {
-        
-        public delegate void progressDelegate();
-        progressDelegate Delegate;
+
+        ProgressSubject slowWorker;
 
         public Form1()
         {
             InitializeComponent();
+            slowWorker = new ProgressSubject();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            reset();
-            if (SpinRB.Checked)
-            {
-                Delegate = new progressDelegate(incSpin);
-            }
-            else if (ProgressRB.Checked)
-            {
-                Delegate = new progressDelegate(incProg);
-            }
-            else if (TrackRB.Checked)
-            {
-                Delegate = new progressDelegate(incTrack);
-            }
-            slowMethod(Delegate);
+            slowWorker.SlowMethod();
         }
-        private void slowMethod(progressDelegate Delegate)
-        {
-            for (int i = 0; i < 100; i++)
-            {
-                Thread.Sleep(40);
-                Delegate();
-            }
-        }
-        private void reset()
-        {
-            progressBar1.Value = 0;
-            numericUpDown1.Value = 0;
-            trackBar1.Value = 0;
-        }
-        private void incSpin()
-        {
-            Application.DoEvents();
-            numericUpDown1.Value++;
-        }
+        //private void reset()
+        //{
+        //    progressBar1.Value = 0;
+        //    numericUpDown1.Value = 0;
+        //    trackBar1.Value = 0;
+        //}
+        //private void incSpin()
+        //{
+        //    Application.DoEvents();
+        //    numericUpDown1.Value++;
+        //}
 
-        private void incProg()
-        {
-            progressBar1.Value++;
-        }
+        //private void incProg()
+        //{
+        //    progressBar1.Value++;
+        //}
 
-        private void incTrack()
-        {
-            trackBar1.Value++;
-        }
+        //private void incTrack()
+        //{
+        //    trackBar1.Value++;
+        //}
     }
 }
 
---
+++ DelegatesProgressEvents/DelegatesProgress/ProgressSubject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesProgress
+{
+    public class ProgressSubject
+    {
+        //observers bind code to this event
+        public event EventHandler OnUpdateEvent;
+
+        public void SlowMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                System.Threading.Thread.Sleep(500);
+                OnUpdateEvent();
+            }
+        }
+    }
+}
