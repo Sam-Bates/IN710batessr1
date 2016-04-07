c4511fc95afe59a7fcb1b42d00c48093ea4e119d
Sam-Bates <batez1993@gmail.com>
2016-04-07 18:55:55 +1200

    Observer base

---
+++ DelegatesProgressEvents/DelegatesProgress/ObserverBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesProgress
+{
+    public abstract class ObserverBase
+    {
+        ProgressSubject subject;
+        public ObserverBase(ProgressSubject subject)
+        {
+            this.subject = subject;
+
+            EventHandler handler = new EventHandler(EventCode);
+
+            subject.OnUpdateEvent += handler;
+        }
+        public abstract void EventCode(object obj, EventArgs e);
+    }
+}
