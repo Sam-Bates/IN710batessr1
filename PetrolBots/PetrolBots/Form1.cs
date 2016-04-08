928de2d3a5e6d780da818923d10e8ea5357a2ab3
Sam-Bates <batez1993@gmail.com>
2016-04-08 13:16:56 +1200

    drawing somewhat done

---
+++ PetrolBots/PetrolBots/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PetrolBots
+{
+    public partial class Form1 : Form
+    {
+        public const int SHIP_SIZE = 50;
+        public const int BOT_SIZE = 10;
+
+        
+        Graphics mainCanvas;
+        List<PetrolBot> botList;
+        List<Ship> shipList;
+        Random rand;
+        Brush backgroundBrush;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            mainCanvas = CreateGraphics();
+            backgroundBrush = new SolidBrush(Color.White);
+
+            botList = new List<PetrolBot>();
+            shipList = new List<Ship>();
+
+            rand = new Random();
+
+            Ship s1 = new Ship(SHIP_SIZE, mainCanvas, rand);
+
+            shipList.Add(s1);
+
+        }
+
+        public void tick()
+        {
+            mainCanvas.FillRectangle(backgroundBrush, 0, 0, 500, 500);
+
+            //Move & redraw all ships in the list
+            for (int i = 0; i < shipList.Count; i++)
+            {
+                shipList[i].moveShip();
+                shipList[i].drawShip();
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            tick();
+        }
+    }
+}
+
---
+++ PetrolBots/PetrolBots/Ship.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetrolBots
+{
+    public class Ship
+    {
+        public delegate void OutOfFuelEventHandler(object shipSubject, EventArgs e);
+        public event OutOfFuelEventHandler OutOfFuelEvent;
+
+        public Point ShipLocation;
+        public Graphics mainCanvas;
+        public int size;
+        public Random rand;
+        public Color shipColour;
+        public int petrol;
+
+        public Ship(int size, Graphics mainCanvas, Random rand)
+        {
+            this.size = size;
+            this.mainCanvas = mainCanvas;
+            this.rand = rand;
+            ShipLocation.X = 50;
+            ShipLocation.Y = 50;
+        }
+        public void moveShip()
+        {
+            int random = rand.Next(3);
+            if (random == 0)
+            {
+                ShipLocation.X++;
+            }
+            else if (random == 1)
+            {
+                ShipLocation.X--;
+            }
+            else if (random == 2)
+            {
+                ShipLocation.Y++;
+            }
+            else if (random == 3)
+            {
+                ShipLocation.Y--;
+            }
+        }
+        public void drawShip()
+        {
+
+            shipColour = Color.FromArgb(100, 0, 0);
+
+            SolidBrush shipBrush = new SolidBrush(shipColour);
+
+            mainCanvas.FillRectangle(shipBrush, ShipLocation.X, ShipLocation.Y, size, size);
+        }
+        public void refuel()
+        {
+            petrol++;
+        }
+    }
+}
