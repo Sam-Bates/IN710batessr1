773b8d01e6f550a650b641a2c6e8b05a0a9b6305
Sam-Bates <batez1993@gmail.com>
2016-05-19 12:05:44 +1200

    Completed MVC holiday project

--- Week 12/MVCDemo/MVCDemo/Controllers/HolidayController.cs
+++ Week 12/MVCDemo/MVCDemo/Controllers/HolidayController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCDemo.Models;
 
 namespace MVCDemo.Controllers
 {
@@ -11,7 +12,37 @@ namespace MVCDemo.Controllers
         // GET: Holiday
         public ActionResult Index()
         {
-            return View();
+            int nHolidays = 3;
+            HolidayModel target;
+
+
+            HolidayModel halloween = new HolidayModel("Halloween", new DateTime(DateTime.Now.Year, 10, 31), "http://www.foreignersinpoland.com/wp-content/uploads/2014/09/halloween-pumpkin.jpg");
+
+            HolidayModel boxingDay = new HolidayModel("Boxing Day", new DateTime(DateTime.Now.Year, 12, 26), "http://i.telegraph.co.uk/multimedia/archive/01795/ali_1795027b.jpg");
+
+            HolidayModel queensBirthday = new HolidayModel("Queens Birthday", new DateTime(DateTime.Now.Year, 06, 06), "http://www.crossfitanarchy.com.au/uploads/1/3/0/8/13080067/7638354_orig.jpg");
+
+
+            Random r = new Random();
+
+            target = new HolidayModel();//this seems very bad lol
+
+            switch (r.Next(nHolidays))//choose random holiday and switch
+            {
+                case 0:
+                    target = halloween;
+                    break;
+                case 1:
+                    target = boxingDay;
+                    break;
+                case 2:
+                    target = queensBirthday;
+                    break;
+            }
+
+            target.DaysUntil = (int)(target.Date - DateTime.Now).TotalDays;
+
+            return View(target);
         }
     }
 }
---
+++ Week 12/MVCDemo/MVCDemo/Models/HolidayModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    public class HolidayModel
+    {
+        public String Name { get; set; }
+        public int DaysUntil { get; set; }
+        public String Url { get; set; }
+        public DateTime Date { get; set; }
+        public HolidayModel(String name, DateTime date, String url)
+        {
+            Name = name;
+            Date = date;
+            Url = url;
+        }
+        public HolidayModel() { }
+    }
+}
